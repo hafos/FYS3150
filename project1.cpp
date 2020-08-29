11013f3e1858be85332f110dba247932ab949065
Oskar Hafstad <oskarhaf@student.matnat.uio.no>
2020-08-29 22:02:15 +0200

    Made header file and main file for the first project.

---
+++ project1.cpp
@@ -0,0 +1,4 @@
+/* C++ program solving the one-dimentional Poisson equation
+   with Dirchlet boundary conditions written as a set of linear equations  */
+
+#inculde <project1.h>
---
+++ project1.h
@@ -0,0 +1,8 @@
+/* Header for C++ program solving the one-dimentional Poisson equation
+   with Dirchlet boundary conditions written as a set of linear equations */
+
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+using namespace std;
